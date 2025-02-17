b6218f50b0a40455ee22376b2a55eda61452f442
root <root@Asus-tuf-f15.>
2025-02-17 14:09:20 +0000

    init

--- src/ethereum/EthToken.sol
+++ src/ethereum/EthToken.sol
@@ -4,10 +4,10 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract EthToken is ERC20{
-    constructor() ERC20("Ethereum","ETH"){
+    constructor() ERC20("Ethereum","ETH") Ownable(msg.sender) {
     }
 
-    function mint(address _to , uint _amount)private{
+    function mint(address _to , uint _amount)private onlyOwner { 
         _mint(_to, _amount);
     }
 }
